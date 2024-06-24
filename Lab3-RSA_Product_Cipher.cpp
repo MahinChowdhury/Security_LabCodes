1d122bd6e3a81797511a18f998cda0c87fa1887b
MahinChowdhury <mahinrchowdhury@gmail.com>
2024-06-24 22:50:29 +0600

    RSA product cipher code is fixed

--- Lab3-RSA_Product_Cipher.cpp
+++ Lab3-RSA_Product_Cipher.cpp
@@ -4,64 +4,77 @@
 using namespace std;
 using namespace boost::multiprecision;
 
+cpp_int qpow(cpp_int a, cpp_int b, cpp_int mod){
+
+    a %= mod;
+    cpp_int res = 1;
+    while(b){
+        if(b&1) res = (res*a)%mod;
+        a = (a*a)%mod;
+        b/=2;
+    }
+
+    return res%mod;
+}
+
 int main() {
-	int1024_t p, q, n, phi, i, j, e, d, m1, m2, m=1, mm1=1, mm2=1, cc =1,  c1, c2, cc1 = 1, cc2 = 1, c;
-	 
-	cin>>p>>q;
-	
-	n = p * q;
-	phi = (p-1)*(q-1);
-	
-	i = 2;
-	
-	for(i; i<phi; i++) {
+
+	cpp_int p("22953686867719691230002707821868552601124472329079");
+	cpp_int q("30762542250301270692051460539586166927291732754961");
+
+	cout<<"Enter first message : ";
+	cpp_int m1;cin>>m1;
+	cout<<"Enter second message : ";
+	cpp_int m2;cin>>m2;
+
+	cpp_int n = p * q;
+	cpp_int phi = (p-1)*(q-1);
+
+	cpp_int e,d;
+	cpp_int i;
+
+	for(i=2 ; i<phi; i++) {
 		if(gcd(i, phi)==1) {
 			e = i;
 			break;
 		}
 	}
-	
+
 	cout<<"e : "<<e<<endl;
-	
+
 	for(i = 1; ; i++) {
 		if((1+i*phi)%e == 0) {
 			d = (1+i*phi)/e;
 			break;
 		}
 	}
-	
+
 	cout<<"d : "<<d<<endl;
-	
-	m1 = 12;
-	m2 = 15;
-	
-	cout<<"m1, m2, m = "<<m1<<", "<<m2<<", "<<m1*m2<<endl;
-	
-	for(int i=1; i<=e; i++) {
-		mm1 *= m1;
-		mm2 *= m2;
-	}
-	
-	c1 = mm1 % n;
-	c2 = mm2 % n;
-	
-	c = (mm1 * mm2) % n;
-	
-	cout<<"c = "<<c<<", c1 = "<<c1<<", c2 = "<<c2<<endl;	
-	
-	for(int i=1; i<=d; i++) {
-		cc1 *= c1;
-		cc2 *= c2;
-		cc *= c;
-	}
-	
-	// cout<<cc2<<endl;
-	
-	m1 = cc1 % n;
-	m2 = cc2 % n;
-	m = cc % n;
-	
-	cout<<"m = "<<m<<", m1 = "<<m1<<", m2 = "<<m2<<endl;;
+
+
+	cpp_int c1 = qpow(m1,e,n);
+	cpp_int c2 = qpow(m2,e,n);
+
+    //Get m1*m2 from prodcut cipher C.
+	cpp_int c = c1*c2;
+	cpp_int mm_dec = qpow(c,d,n);
+
+	//Get m1 and m2 from c1 and c2
+
+	cpp_int m1_old = qpow(c1,d,n);
+	cpp_int m2_old = qpow(c2,d,n);
+
+	//Product of m1 and m2 which we decrypted from c1 , c2
+	cpp_int mm_old = m1_old*m2_old;
+
+
+
+	cout<<mm_old<<" "<<mm_dec<<endl;
+
+	if(mm_old == mm_dec) printf("Matched.\n");
+	else printf("Didn\'nt matched.\n");
+
+
 }
 
 
